209da896fcbbdf9a32ff747ab7537b3afef8534b
Misskhat <misskhat.hossen@gmail.com>
2026-01-22 00:26:03 +0600

    nav link added

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => {
     return (
         <div>
-            <button>styleDecor</button>
+            <button className='px-4 py-2 bg-linear-to-r from-[#090979] to-[#00D4FF] text-white font-semibold rounded'>styleDecor</button>
         </div>
     );
 };
