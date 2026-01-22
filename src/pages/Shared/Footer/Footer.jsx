becc794a9df51085977cb9fbfb1fae0ce77fe483
Misskhat <misskhat.hossen@gmail.com>
2026-01-22 18:53:51 +0600

    navbar and footer design done

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -7,40 +7,47 @@ import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
-            <aside>
-                <Logo></Logo>
-                <p>
-                    <span className='font-bold'>STYLEDECOR</span>
-                    <br />
-                    17 years of evolving in interior design, with a passion for meaningful spaces.
-                </p>
-            </aside>
-            <nav>
-                <h6 className="footer-title">Our Services</h6>
-                <a className="link link-hover">Residential | Home</a>
-                <a className="link link-hover">Office Design</a>
-                <a className="link link-hover">Showroom Design</a>
-                <a className="link link-hover">Landscape Architecture</a>
-                <a className="link link-hover">Exterior Design</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Contact Us</h6>
-                <a className="link link-hover">323 Aftab Tower, 1st Floor, <br />Rampura, Dhaka-1219.</a>
-                <a className="link link-hover">+8801754804496</a>
-                <a className="link link-hover">misskhat.hossen@gmail.com</a>
+        <div className='bg-base-200'>
+            <footer className="footer sm:footer-horizontal w-11/12 mx-auto text-base-content py-7 px-2">
+                <aside>
+                    <Logo></Logo>
+                    <p>
+                        <span className='font-bold'>STYLEDECOR</span>
+                        <br />
+                        17 years of evolving in interior design, with a passion for meaningful spaces.
+                    </p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Our Services</h6>
+                    <a className="link link-hover">Residential | Home</a>
+                    <a className="link link-hover">Office Design</a>
+                    <a className="link link-hover">Showroom Design</a>
+                    <a className="link link-hover">Landscape Architecture</a>
+                    <a className="link link-hover">Exterior Design</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Contact Us</h6>
+                    <a className="link link-hover">323 Aftab Tower, 1st Floor, <br />Rampura, Dhaka-1219.</a>
+                    <a className="link link-hover">+8801754804496</a>
+                    <a className="link link-hover">misskhat.hossen@gmail.com</a>
 
-            </nav>
-            <nav>
-                <h6 className="footer-title">Connect With Us</h6>
-                <div className='flex gap-2 text-2xl'>
-                    <FaFacebookSquare />
-                    <FaYoutubeSquare />
-                    <FaInstagramSquare />
-                    <FaLinkedin />
-                </div>
-            </nav>
-        </footer>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Connect With Us</h6>
+                    <div className='flex gap-2 text-2xl'>
+                        <FaFacebookSquare />
+                        <FaYoutubeSquare />
+                        <FaInstagramSquare />
+                        <FaLinkedin />
+                    </div>
+                </nav>
+            </footer>
+            <div className="footer sm:footer-horizontal footer-center bg-base-200 text-base-content p-4 border-t border-t-gray-300">
+                <aside>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved by <span className='font-semibold'>STYLEDECOR</span></p>
+                </aside>
+            </div>
+        </div>
     );
 };
 
