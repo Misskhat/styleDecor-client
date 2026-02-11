2b019ab7a854da717fb72965cded85a96c9de2bd
Misskhat <misskhat.hossen@gmail.com>
2026-02-11 13:18:09 +0600

    login and register form added react hook form and validate implement

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,48 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
+import { FcGoogle } from "react-icons/fc";
+
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const handleLogin = (data) => {
+        console.log(data);
+    }
     return (
         <div>
             <div className="flex items-center justify-center min-h-screen">
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
                         <h3 className='text-center text-2xl text-[#090979] font-bold p-2 rounded-3xl border-t-2 border-[#00d4ff]'>Welcome to styleDecor</h3>
-                        <form>
+                        <form onSubmit={handleSubmit(handleLogin)}>
                             <fieldset className="fieldset">
                                 <label className="label">Email</label>
-                                <input type="email" className="input" placeholder="Email" />
+                                <input type="email" className="input" {...register("email", { required: true })} placeholder="Email" />
+                                {
+                                    errors.email?.type === "required" &&
+                                    <p className='text-red-500'>Please provide your email id.</p>
+                                }
                                 <label className="label">Password</label>
-                                <input type="password" className="input" placeholder="Password" />
+                                <input type="password" className="input" {...register("password", { required: true, minLength: 6 })} placeholder="Password" />
+                                {
+                                    errors.password?.type === "required" &&
+                                    <p className='text-red-500'>Password required</p>
+                                }
+                                {
+                                    errors.password?.type === "minLength" &&
+                                    <p className='text-red-500'>Password must be 6 character.</p>
+                                }
                                 <div><a className="link link-hover">Forgot password?</a></div>
                                 <button className="btn mt-4 bg-linear-to-r from-[#090979] to-[#00D4FF] text-white">Login</button>
-                                <p>Don't have any account? <Link to={"/registration"} className='cursor-pointer'><span className='font-bold text-blue-500'>Register</span></Link></p>
                             </fieldset>
                         </form>
+                        <button className="btn bg-white text-black border-[#e5e5e5]">
+                            <FcGoogle className='text-2xl' />
+                            Login with Google
+                        </button>
+                        <p>Don't have any account? <Link to={"/registration"} className='cursor-pointer'><span className='font-bold text-blue-500'>Register</span></Link></p>
                     </div>
                 </div>
             </div>
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,29 +1,57 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { FcGoogle } from 'react-icons/fc';
 import { Link } from 'react-router';
 
 const Registration = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const handleRegister = (data) => {
+        console.log(data);
+    }
+
     return (
         <div>
             <div className="flex items-center justify-center min-h-screen">
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
                         <h3 className='text-center text-2xl text-[#090979] font-bold p-2 rounded-3xl border-t-2 border-[#00d4ff]'>Welcome to styleDecor</h3>
-                        <form>
+                        <form onSubmit={handleSubmit(handleRegister)}>
                             <fieldset className="fieldset">
                                 <label className="label">Name</label>
-                                <input type="text" className="input" placeholder="Name" />
+                                <input type="text" className="input" {...register('name', { required: true })} placeholder="Name" />
+                                {
+                                    errors.name?.type === "required" &&
+                                    <p className='text-red-500'>Name must be require</p>
+                                }
                                 <label className="label">Mobile Number</label>
-                                <input type="number" className="input" placeholder="Mobile Number" />
+                                <input type="number" className="input" {...register('mobileNumber')} placeholder="Mobile Number" />
                                 <label className="label">Upload Your Photo</label>
                                 <input type="file" className="file-input" />
                                 <label className="label">Email</label>
-                                <input type="email" className="input" placeholder="Email" />
+                                <input type="email" className="input" {...register('email', { required: true })} placeholder="Email" />
+                                {
+                                    errors.email?.type === 'required' &&
+                                    <p className='text-red-500'>Email ID required</p>
+                                }
                                 <label className="label">Password</label>
-                                <input type="password" className="input" placeholder="Password" />
-                                <button className="btn mt-4 bg-linear-to-r from-[#090979] to-[#00D4FF] text-white">Login</button>
-                                <p>Have an Account? <Link to={"/login"} className='cursor-pointer'><span className='font-bold text-blue-500'>Login</span></Link></p>
+                                <input type="password" className="input" {...register('password', { required: true, minLength: 6 })} placeholder="Password" />
+                                {
+                                    errors.password?.type === 'required' &&
+                                    <p className='text-red-500'>Password Require</p>
+                                }
+                                {
+                                    errors.password?.type === 'minLength' &&
+                                    <p className='text-red-500'>Password must be 6 character.</p>
+                                }
+                                <button className="btn mt-4 bg-linear-to-r from-[#090979] to-[#00D4FF] text-white">Register</button>
                             </fieldset>
                         </form>
+                        <button className="btn bg-white text-black border-[#e5e5e5]">
+                            <FcGoogle className='text-2xl' />
+                            Login with Google
+                        </button>
+                        <p>Have an Account? <Link to={"/login"} className='cursor-pointer'><span className='font-bold text-blue-500'>Login</span></Link></p>
                     </div>
                 </div>
             </div>
