30bcbababea0f4d51ab4fcebd98543603ae8720f
Misskhat <misskhat.hossen@gmail.com>
2026-02-13 21:08:14 +0600

    auth creation

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router/dom";
 import { router } from './routes/router';
+import AuthProvider from './Context/AuthProvider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
