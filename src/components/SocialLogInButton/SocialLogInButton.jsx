c601338a08e455dcc18e5481bd4b15d555f8ed80
Misskhat <misskhat.hossen@gmail.com>
2026-05-06 09:25:33 +0600

    axios instant created

--- src/components/SocialLogInButton/SocialLogInButton.jsx
+++ src/components/SocialLogInButton/SocialLogInButton.jsx
@@ -1,32 +1,41 @@
-import React from 'react';
-import { FcGoogle } from 'react-icons/fc';
-import useAuth from '../../hooks/useAuth';
-import { toast } from 'react-toastify';
-import { useLocation, useNavigate } from 'react-router';
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import useAuth from "../../hooks/useAuth";
+import { toast } from "react-toastify";
+import { useLocation, useNavigate } from "react-router";
+import { axiosInstance } from "../../utility/axiosInstance";
 
 const SocialLogInButton = () => {
+  const { signInGoogle } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    const { signInGoogle } = useAuth()
-    const location = useLocation()
-    const navigate = useNavigate()
+  const handleSignInGoogle = () => {
+    signInGoogle()
+      .then((result) => {
+        console.log(result.user);
+        axiosInstance
+          .post("/api/users/login", {
+            name: result.user.displayName,
+            email: result.user.email,
+          })
+          .then(() => {
+            navigate(location?.state || "/");
+            toast.success("Welcome! and thank you for log in");
+          });
+      })
+      .catch((error) => console.log(error));
+  };
 
-    const handleSignInGoogle = () => {
-        signInGoogle()
-            .then(result => {
-                console.log(result.user);
-                navigate(location?.state || '/')
-                toast.success('Welcome! and thank you for log in')
-
-            })
-            .catch(error => console.log(error))
-    }
-
-    return (
-        <button onClick={handleSignInGoogle} className="btn bg-white text-black border-[#e5e5e5]">
-            <FcGoogle className='text-2xl' />
-            Login with Google
-        </button>
-    );
+  return (
+    <button
+      onClick={handleSignInGoogle}
+      className="btn bg-white text-black border-[#e5e5e5]"
+    >
+      <FcGoogle className="text-2xl" />
+      Login with Google
+    </button>
+  );
 };
 
 export default SocialLogInButton;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,63 +1,95 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { Link, useLocation, useNavigate } from 'react-router';
-import useAuth from '../../hooks/useAuth';
-import SocialLogInButton from '../../components/SocialLogInButton/SocialLogInButton';
-import { toast } from 'react-toastify';
-
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link, useLocation, useNavigate } from "react-router";
+import useAuth from "../../hooks/useAuth";
+import SocialLogInButton from "../../components/SocialLogInButton/SocialLogInButton";
+import { toast } from "react-toastify";
+import { axiosInstance } from "../../utility/axiosInstance";
 
 const Login = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm()
-    const { signInUser } = useAuth()
-    const location = useLocation()
-    const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { signInUser } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    const handleLogin = (data) => {
-        console.log(data);
-        signInUser(data.email, data.password)
-            .then(result => {
-                console.log(result.user);
-                navigate(location?.state || '/')
-                toast.success('Welcome! and thank you for log in')
-            })
-            .catch(error => console.log(error))
-    }
-    return (
-        <div>
-            <div className="flex items-center justify-center min-h-screen">
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <h3 className='text-center text-2xl text-[#090979] font-bold p-2 rounded-3xl border-t-2 border-[#00d4ff]'>Welcome to styleDecor</h3>
-                        <form onSubmit={handleSubmit(handleLogin)}>
-                            <fieldset className="fieldset">
-                                <label className="label">Email</label>
-                                <input type="email" className="input" {...register("email", { required: true })} placeholder="Email" />
-                                {
-                                    errors.email?.type === "required" &&
-                                    <p className='text-red-500'>Please provide your email id.</p>
-                                }
-                                <label className="label">Password</label>
-                                <input type="password" className="input" {...register("password", { required: true, minLength: 6 })} placeholder="Password" />
-                                {
-                                    errors.password?.type === "required" &&
-                                    <p className='text-red-500'>Password required</p>
-                                }
-                                {
-                                    errors.password?.type === "minLength" &&
-                                    <p className='text-red-500'>Password must be 6 character.</p>
-                                }
-                                <div><a className="link link-hover">Forgot password?</a></div>
-                                <button className="btn mt-4 bg-linear-to-r from-[#090979] to-[#00D4FF] text-white">Login</button>
-                            </fieldset>
-                        </form>
-                        <SocialLogInButton></SocialLogInButton>
-                        <p>Don't have any account? <Link state={location.state} to={"/registration"} className='cursor-pointer'><span className='font-bold text-blue-500'>Register</span></Link></p>
-                    </div>
+  const handleLogin = (data) => {
+    console.log(data);
+    signInUser(data.email, data.password)
+      .then((result) => {
+        console.log(result.user);
+        axiosInstance
+          .post("/api/users/login", {
+            name: result.user.displayName,
+            email: result.user.email,
+          })
+          .then(() => {
+            toast.success("Welcome! and thank you for log in");
+            navigate(location?.state || "/");
+          });
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <div>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <h3 className="text-center text-2xl text-[#090979] font-bold p-2 rounded-3xl border-t-2 border-[#00d4ff]">
+              Welcome to styleDecor
+            </h3>
+            <form onSubmit={handleSubmit(handleLogin)}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  className="input"
+                  {...register("email", { required: true })}
+                  placeholder="Email"
+                />
+                {errors.email?.type === "required" && (
+                  <p className="text-red-500">Please provide your email id.</p>
+                )}
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  {...register("password", { required: true, minLength: 6 })}
+                  placeholder="Password"
+                />
+                {errors.password?.type === "required" && (
+                  <p className="text-red-500">Password required</p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p className="text-red-500">Password must be 6 character.</p>
+                )}
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
                 </div>
-            </div>
+                <button className="btn mt-4 bg-linear-to-r from-[#090979] to-[#00D4FF] text-white">
+                  Login
+                </button>
+              </fieldset>
+            </form>
+            <SocialLogInButton></SocialLogInButton>
+            <p>
+              Don't have any account?{" "}
+              <Link
+                state={location.state}
+                to={"/registration"}
+                className="cursor-pointer"
+              >
+                <span className="font-bold text-blue-500">Register</span>
+              </Link>
+            </p>
+          </div>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
